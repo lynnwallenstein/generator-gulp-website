63e4edcc161fd4489cbee911d5232aa37df231a6
Lynn Wallenstein <lynn.wallenstein@github.com>
2014-11-02 19:00:28 -0800

    Yeoman generator

--- app/index.js
+++ app/index.js
@@ -5,6 +5,7 @@ var yeoman = require("yeoman-generator");
 var yosay = require("yosay");
 
 var GulpWebsiteGenerator = yeoman.generators.Base.extend({
+
   initializing: function () {
     this.pkg = require("../package.json");
   },
@@ -30,12 +31,30 @@ var GulpWebsiteGenerator = yeoman.generators.Base.extend({
   },
 
   writing: {
+
+    gulpfile: function() {
+      this.template('gulpfile.js');
+    },
+
     app: function () {
       this.dest.mkdir("app");
       this.dest.mkdir("app/templates");
+      this.dest.mkdir("app/coffeescript");
+      this.dest.mkdir("app/sass");
+      this.dest.mkdir("app/img");
+      this.dest.mkdir("app/css");
+      this.dest.mkdir("app/js");
+      this.dest.mkdir("app/fonts");
 
       this.src.copy("_package.json", "package.json");
+      this.src.copy("bowerrc", ".bowerrc");
       this.src.copy("_bower.json", "bower.json");
+    },
+
+    sitemeta: function () {
+      this.src.copy("favicon.ico", "app/favicon.ico");
+      this.src.copy("robots.txt", "app/robots.txt");
+      this.src.copy("humans.txt", "app/humans.txt");
     },
 
     projectfiles: function () {
@@ -43,6 +62,7 @@ var GulpWebsiteGenerator = yeoman.generators.Base.extend({
       this.src.copy("jshintrc", ".jshintrc");
       this.src.copy("_scss-lint.yml", ".scss-lint.yml");
     }
+
   },
 
   end: function () {
