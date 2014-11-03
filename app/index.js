c999f4bb610b537a3235a45e79a0656db4cf9317
Lynn Wallenstein <lynn.wallenstein@github.com>
2014-11-02 18:31:46 -0800

    Added scss-lint to templates and setup for Yeoman

--- app/index.js
+++ app/index.js
@@ -1,12 +1,12 @@
-'use strict';
-var util = require('util');
-var path = require('path');
-var yeoman = require('yeoman-generator');
-var yosay = require('yosay');
+"use strict";
+var util = require("util");
+var path = require("path");
+var yeoman = require("yeoman-generator");
+var yosay = require("yosay");
 
 var GulpWebsiteGenerator = yeoman.generators.Base.extend({
   initializing: function () {
-    this.pkg = require('../package.json');
+    this.pkg = require("../package.json");
   },
 
   prompting: function () {
@@ -14,14 +14,12 @@ var GulpWebsiteGenerator = yeoman.generators.Base.extend({
 
     // Have Yeoman greet the user.
     this.log(yosay(
-      'Welcome to the prime GulpWebsite generator!'
+      "Welcome Lynn\'s Yeoman web site generator!"
     ));
 
     var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
+      name: "websiteName",
+      message: "What is your website\'s name ?"
     }];
 
     this.prompt(prompts, function (props) {
@@ -33,16 +31,17 @@ var GulpWebsiteGenerator = yeoman.generators.Base.extend({
 
   writing: {
     app: function () {
-      this.dest.mkdir('app');
-      this.dest.mkdir('app/templates');
+      this.dest.mkdir("app");
+      this.dest.mkdir("app/templates");
 
-      this.src.copy('_package.json', 'package.json');
-      this.src.copy('_bower.json', 'bower.json');
+      this.src.copy("_package.json", "package.json");
+      this.src.copy("_bower.json", "bower.json");
     },
 
     projectfiles: function () {
-      this.src.copy('editorconfig', '.editorconfig');
-      this.src.copy('jshintrc', '.jshintrc');
+      this.src.copy("editorconfig", ".editorconfig");
+      this.src.copy("jshintrc", ".jshintrc");
+      this.src.copy("_scss-lint.yml", ".scss-lint.yml");
     }
   },
 
