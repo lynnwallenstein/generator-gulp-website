419ce2c16d07ee5944150ce7ab248c352347aa9b
Lynn Wallenstein <lynn.wallenstein@github.com>
2014-11-25 02:39:17 -0500

    indenting cleanup

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -9,48 +9,54 @@ gulp.task("clean", require("del").bind(null, [".tmp", "dist"]));
 
 gulp.task("html", function() {
   return gulp.src(["app/*.html"])
-  .pipe(fileinclude({prefix: "@@", basepath: "app/partials/"}))
-  .pipe(gulp.dest(".tmp/"));
+    .pipe(fileinclude({prefix: "@@", basepath: "app/partials/"}))
+    .pipe(gulp.dest(".tmp/"));
 });
 
 gulp.task("styles", function () {
-  return gulp.src("app/sass/main.scss")
-  .pipe($.plumber())
-  .pipe($.rubySass({
-    style: "expanded",
-    precision: 10
-  }))
-  .pipe($.autoprefixer({browsers: ["last 1 version"]}))
-  .pipe(gulp.dest("app/css"));
+  return gulp.src("app/sass/*.scss")
+    .pipe($.plumber())
+    .pipe($.rubySass({
+      style: "expanded",
+      precision: 10
+    }))
+    .pipe($.autoprefixer({browsers: ["last 1 version"]}))
+    .pipe(gulp.dest("app/css"));
 });
 
 gulp.task("scripts", function() {
   var gutil = require("gulp-util")
-  return gulp.src("app/coffeescript/**/*.coffee")
-  .pipe($.plumber())
-  .pipe($.sourcemaps.init())
-  .pipe($.coffee({bare: true}).on("error", gutil.log))
-  .pipe($.sourcemaps.write())
-  .pipe(gulp.dest("app/js"))
+  return gulp.src("app/coffeescript/**")
+    .pipe($.plumber())
+    .pipe($.sourcemaps.init())
+    .pipe($.coffee({bare: true}).on("error", gutil.log))
+    .pipe($.sourcemaps.write())
+    .pipe(gulp.dest("app/js"))
 })
+
+gulp.task("images", function () {
+  return gulp.src("app/images/**/*")
+    .pipe($.cache($.imagemin({progressive: true, interlaced: true})))
+    .pipe(gulp.dest("app/img"));
+});
 
 gulp.task("connect", ["html", "styles", "scripts"], function () {
   var serveStatic = require("serve-static");
   var serveIndex = require("serve-index");
   var app = require("connect")()
-  .use(require("connect-livereload")({port: 35729}))
-  .use(serveStatic(".tmp"))
-  .use(serveStatic("app"))
-  // paths to bower_components should be relative to the current file
-  // e.g. in app/index.html you should use ../bower_components
-  .use("/bower_components", serveStatic("bower_components"))
-  .use(serveIndex(".tmp"));
+    .use(require("connect-livereload")({port: 35729}))
+    .use(serveStatic(".tmp"))
+    .use(serveStatic("app"))
+    // paths to bower_components should be relative to the current file
+    // e.g. in app/index.html you should use ../bower_components
+    .use("/bower_components", serveStatic("bower_components"))
+    .use(serveIndex(".tmp"));
 
   require("http").createServer(app)
-  .listen(9000)
-  .on("listening", function () {
-    console.log("Started connect web server on http://localhost:9000");
-  });
+    .listen(9000)
+    .on("listening", function () {
+      console.log("Started connect web server on http://localhost:9000");
+    });
 });
 
 gulp.task("serve", ["connect"], function () {
@@ -59,37 +65,32 @@ gulp.task("serve", ["connect"], function () {
 
 gulp.task("watch", ["serve"], function () {
   $.livereload.listen();
+  gulp.watch("app/*.html", "app/partials/**", ["html"]).on("change", $.livereload.changed);
+  gulp.watch("app/sass/**/*.scss", ["styles"]).on("change", $.livereload.changed);
+  gulp.watch("app/img/**", ["images"]).on("change", $.livereload.changed);
+  gulp.watch("app/coffeescript/**/*.coffee", ["scripts"]).on("change", $.livereload.changed);
+  gulp.watch("bower.json", ["wiredep"]);
+});
 
-  // watch for changes
-  gulp.watch([
-    "app/*.html",
-    "app/images/**/*"
-    ]).on("change", $.livereload.changed);
+gulp.task("build", ["clean", "html", "styles", "scripts", "images"], function () {
 
-    gulp.watch("app/sass/**/*.scss", ["styles"]).on("change", $.livereload.changed);
-    gulp.watch("app/coffeescript/**/*.coffee", ["scripts"]).on("change", $.livereload.changed);
-    gulp.watch("bower.json", ["wiredep"]);
-  });
-
-  gulp.task("build", ["clean", "html", "styles", "scripts"], function () {
-
-    gulp.src([".tmp/*.html"], { dot: true })
+  gulp.src([".tmp/*.html"], { dot: true })
     .pipe(gulp.dest("dist"));
 
-    gulp.src(["app/img/**"], { dot: true })
+  gulp.src(["app/img/**"], { dot: true })
     .pipe(gulp.dest("dist/img"));
 
-    gulp.src(["app/css/**"], { dot: true })
+  gulp.src(["app/css/**"], { dot: true })
     .pipe(gulp.dest("dist/css"));
 
-    gulp.src(["app/js/**"], { dot: true })
+  gulp.src(["app/js/**"], { dot: true })
     .pipe(gulp.dest("dist/js"));
 
-    gulp.src(["app/fonts/**"], { dot: true })
+  gulp.src(["app/fonts/**"], { dot: true })
     .pipe(gulp.dest("dist/fonts"));
 
-  });
+});
 
-  gulp.task("default", function () {
-    gulp.start("build");
-  });
+gulp.task("default", function () {
+  gulp.start("build");
+});
