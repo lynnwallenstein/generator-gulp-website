b19a7a4e63d76ffd85e34a42d436af89ec375afa
Lynn Wallenstein <lynn.wallenstein@github.com>
2014-11-26 17:09:23 -0500

    I can has minification and versioning

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -3,9 +3,9 @@
 
 var gulp = require("gulp");
 var $ = require("gulp-load-plugins")();
-var fileinclude = require("gulp-file-include");
-var wiredep = require("wiredep").stream;
 var gutil = require("gulp-util")
+var fileinclude = require("gulp-file-include")
+var wiredep = require("wiredep").stream;
 
 gulp.task("clean", require("del").bind(null, [".tmp", "dist"]));
 
@@ -14,19 +14,8 @@ gulp.task("styles", function () {
   return gulp.src("app/sass/*.scss")
     .pipe($.plumber())
     .pipe($.rubySass({style: "expanded", precision: 10}))
-    .pipe($.autoprefixer({browsers: ["last 3 version"]}))
+    .pipe($.autoprefixer({browsers: ["last 3 versions"]}))
     .pipe(gulp.dest("app/css"));
-
-});
-
-gulp.task("markup", ["styles"], function () {
-
-  return gulp.src(["app/*.html"])
-    .pipe($.plumber())
-    .pipe(fileinclude({prefix: "@@", basepath: "app/partials/"}))
-    .pipe(wiredep({directory: "app/bower_components"}))
-    .pipe($.usemin())
-    .pipe(gulp.dest(".tmp/"));
 
 });
 
@@ -36,30 +25,57 @@ gulp.task("scripts", function() {
     .pipe($.plumber())
     .pipe($.sourcemaps.init())
     .pipe($.coffee({bare: true}).on("error", gutil.log))
+    .pipe($.jshint())
+    .pipe($.jshint.reporter("jshint-stylish"))
+    .pipe($.jshint.reporter("fail"))
     .pipe($.sourcemaps.write())
     .pipe(gulp.dest("app/js"));
 
 })
+
+gulp.task("markup", ["styles", "scripts"], function () {
+
+  return gulp.src(["app/*.html"])
+    .pipe($.plumber())
+    .pipe(fileinclude({prefix: "@@", basepath: "app/partials/"}))
+    .pipe(wiredep({directory: "bower_components"}))
+    .pipe($.usemin({
+      css: [$.minifyCss(), $.rev()],
+      html: [$.minifyHtml({empty: true})],
+      js: [$.uglify(), $.rev()]
+    }))
+    .pipe(gulp.dest(".tmp/"));
+
+});
 
 gulp.task("images", function () {
 
   return gulp.src("app/images/**/*")
     .pipe($.plumber())
     .pipe($.cache($.imagemin({progressive: true, interlaced: true})))
-    .pipe(gulp.dest("app/img"));
+    .pipe(gulp.dest(".tmp/img"));
 
 });
 
-gulp.task("connect", ["scripts", "images", "markup"], function () {
+gulp.task("fonts", function () {
+
+  return gulp.src(require("main-bower-files")().concat("app/fonts/**/*"))
+    .pipe($.plumber())
+    .pipe($.filter("**/*.{eot,svg,ttf,woff}"))
+    .pipe($.flatten())
+    .pipe(gulp.dest(".tmp/fonts"));
+
+});
+
+gulp.task("connect", ["fonts", "images", "markup"], function () {
   var serveStatic = require("serve-static");
   var serveIndex = require("serve-index");
   var app = require("connect")()
   .use(require("connect-livereload")({port: 35729}))
   .use(serveStatic(".tmp"))
-  .use(serveStatic("app"))
   // paths to bower_components should be relative to the current file
   // e.g. in app/index.html you should use ../bower_components
-  .use("/app/bower_components", serveStatic("bower_components"))
+  .use("/bower_components", serveStatic("bower_components"))
   .use(serveIndex(".tmp"));
 
   require("http").createServer(app)
@@ -79,38 +95,38 @@ gulp.task("watch", ["serve"], function () {
   gulp.watch(["app/sass/**"], ["markup"]);
   gulp.watch(["app/*.html"], ["markup"]);
   gulp.watch(["app/partials/**"], ["markup"]);
-  gulp.watch("app/img/**", ["images"]).on("change", $.livereload.changed);
-  gulp.watch("app/coffeescript/**/*.coffee", ["scripts"]).on("change", $.livereload.changed);
+  gulp.watch(["app/coffeescript/**"], ["markup"]);
+  gulp.watch(["app/images/**"], ["images"]);
   gulp.watch(".tmp/*").on("change", $.livereload.changed);
 });
 
-gulp.task("build", ["scripts", "images", "markup"], function () {
+gulp.task("build", ["fonts", "images", "markup"], function () {
 
   gulp.src([".tmp/*.html"], { dot: true })
-  .pipe($.plumber())
-  .pipe(gulp.dest("dist"))
-  .pipe($.size({title: "build html", gzip: true}));
+    .pipe($.plumber())
+    .pipe(gulp.dest("dist"))
+    .pipe($.size({title: "build html", gzip: true}));
 
-  gulp.src(["app/img/**"], { dot: true })
-  .pipe($.plumber())
-  .pipe(gulp.dest("dist/img"))
-  .pipe($.size({title: "build images", gzip: true}));
+  gulp.src([".tmp/img/**"], { dot: true })
+    .pipe($.plumber())
+    .pipe(gulp.dest("dist/img"))
+    .pipe($.size({title: "build images", gzip: true}));
 
-  gulp.src([".tmp/css/**"], { dot: true })
-  .pipe($.plumber())
-  .pipe($.if("*.css", $.csso()))
-  .pipe(gulp.dest("dist/css"))
-  .pipe($.size({title: "build css", gzip: true}));
+  gulp.src([".tmp/css/*-*.css"], { dot: true })
+    .pipe($.plumber())
+    .pipe($.if("*.css", $.csso()))
+    .pipe(gulp.dest("dist/css"))
+    .pipe($.size({title: "build css", gzip: true}));
 
-  gulp.src([".tmp/js/**"], { dot: true })
-  .pipe($.plumber())
-  .pipe(gulp.dest("dist/js"))
-  .pipe($.size({title: "build js", gzip: true}));
+  gulp.src([".tmp/js/*-*.js"], { dot: true })
+    .pipe($.plumber())
+    .pipe(gulp.dest("dist/js"))
+    .pipe($.size({title: "build js", gzip: true}));
 
-  gulp.src(["app/fonts/**"], { dot: true })
-  .pipe($.plumber())
-  .pipe(gulp.dest("dist/fonts"))
-  .pipe($.size({title: "build fonts", gzip: true}));
+  gulp.src([".tmp/fonts/**"], { dot: true })
+    .pipe($.plumber())
+    .pipe(gulp.dest("dist/fonts"))
+    .pipe($.size({title: "build fonts", gzip: true}));
 
 });
 
