df9667a3b3c75d4be9f962fbb941b019d0c3210b
Lynn Wallenstein <lynn.wallenstein@github.com>
2014-11-26 00:38:06 -0500

    finally running in the right order

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -5,45 +5,52 @@ var gulp = require("gulp");
 var $ = require("gulp-load-plugins")();
 var fileinclude = require("gulp-file-include");
 var wiredep = require("wiredep").stream;
+var gutil = require("gulp-util")
 
 gulp.task("clean", require("del").bind(null, [".tmp", "dist"]));
 
-gulp.task("html", function() {
-  return gulp.src(["app/*.html"])
-  .pipe($.plumber())
-  .pipe(fileinclude({prefix: "@@", basepath: "app/partials/"}))
-  .pipe(wiredep({directory: "app/bower_components"}))
-  .pipe($.usemin())
-  .pipe(gulp.dest(".tmp/"))
-  .pipe($.size({title: "html", gzip: true}));
+gulp.task("styles", function () {
+
+  return gulp.src("app/sass/*.scss")
+    .pipe($.plumber())
+    .pipe($.rubySass({style: "expanded", precision: 10}))
+    .pipe($.autoprefixer({browsers: ["last 3 version"]}))
+    .pipe(gulp.dest("app/css"));
+
 });
 
-gulp.task("styles", function () {
-  return gulp.src("app/sass/*.scss")
-  .pipe($.plumber())
-  .pipe($.rubySass({style: "expanded", precision: 10}))
-  .pipe($.autoprefixer({browsers: ["last 1 version"]}))
-  .pipe(gulp.dest("app/css"));
+gulp.task("markup", ["styles"], function () {
+
+  return gulp.src(["app/*.html"])
+    .pipe($.plumber())
+    .pipe(fileinclude({prefix: "@@", basepath: "app/partials/"}))
+    .pipe(wiredep({directory: "app/bower_components"}))
+    .pipe($.usemin())
+    .pipe(gulp.dest(".tmp/"));
+
 });
 
 gulp.task("scripts", function() {
-  var gutil = require("gulp-util")
+
   return gulp.src("app/coffeescript/**")
-  .pipe($.plumber())
-  .pipe($.sourcemaps.init())
-  .pipe($.coffee({bare: true}).on("error", gutil.log))
-  .pipe($.sourcemaps.write())
-  .pipe(gulp.dest("app/js"))
+    .pipe($.plumber())
+    .pipe($.sourcemaps.init())
+    .pipe($.coffee({bare: true}).on("error", gutil.log))
+    .pipe($.sourcemaps.write())
+    .pipe(gulp.dest("app/js"));
+
 })
 
 gulp.task("images", function () {
+
   return gulp.src("app/images/**/*")
-  .pipe($.plumber())
-  .pipe($.cache($.imagemin({progressive: true, interlaced: true})))
-  .pipe(gulp.dest("app/img"));
+    .pipe($.plumber())
+    .pipe($.cache($.imagemin({progressive: true, interlaced: true})))
+    .pipe(gulp.dest("app/img"));
+
 });
 
-gulp.task("connect", ["html", "styles", "scripts"], function () {
+gulp.task("connect", ["scripts", "images", "markup"], function () {
   var serveStatic = require("serve-static");
   var serveIndex = require("serve-index");
   var app = require("connect")()
@@ -52,7 +59,7 @@ gulp.task("connect", ["html", "styles", "scripts"], function () {
   .use(serveStatic("app"))
   // paths to bower_components should be relative to the current file
   // e.g. in app/index.html you should use ../bower_components
-  .use("/bower_components", serveStatic("bower_components"))
+  .use("/app/bower_components", serveStatic("bower_components"))
   .use(serveIndex(".tmp"));
 
   require("http").createServer(app)
@@ -69,13 +76,15 @@ gulp.task("serve", ["connect"], function () {
 
 gulp.task("watch", ["serve"], function () {
   $.livereload.listen();
-  gulp.watch("app/*.html", "app/partials/**", ["html"]).on("change", $.livereload.changed);
-  gulp.watch("app/sass/**/*.scss", ["styles"]).on("change", $.livereload.changed);
+  gulp.watch(["app/sass/**"], ["markup"]);
+  gulp.watch(["app/*.html"], ["markup"]);
+  gulp.watch(["app/partials/**"], ["markup"]);
   gulp.watch("app/img/**", ["images"]).on("change", $.livereload.changed);
   gulp.watch("app/coffeescript/**/*.coffee", ["scripts"]).on("change", $.livereload.changed);
+  gulp.watch(".tmp/*").on("change", $.livereload.changed);
 });
 
-gulp.task("build", ["styles", "scripts", "images", "html"], function () {
+gulp.task("build", ["scripts", "images", "markup"], function () {
 
   gulp.src([".tmp/*.html"], { dot: true })
   .pipe($.plumber())
